5ae375e6ab0a2596b001bcb162c04ed5e63b9262
ajam93 <52130390+ajam93@users.noreply.github.com>
2021-07-16 13:52:31 +0430

    add types

---
+++ Editor/Statics/DebugType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinyMCE.Editor.Statics
+{
+    public static class DebugType
+    {
+        public const string Info = "info";
+        public const string Error = "error";
+        public const string Warn = "warn";
+        public const string Log = "log";
+    }
+}
---
+++ Editor/Statics/ThemeType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinyMCE.Editor.Statics
+{
+    public static class ThemeType
+    {
+        public const string Snow = "snow";
+        public const string Bubble = "bubble";
+    }
+}
